06ad5e801e53864f5defce1dfbf55254b06ece31
Chenjeh <126557104+chenjeh@users.noreply.github.com>
2025-11-18 05:41:27 +0330

    Update Find_the_Divisibility_Array_of_a_String.py

--- Find_the_Divisibility_Array_of_a_String.py
+++ Find_the_Divisibility_Array_of_a_String.py
@@ -1,3 +1,6 @@
+# 2025/11/18
+# https://leetcode.com/problems/find-the-divisibility-array-of-a-string/description/
+
 class Solution:
     def divisibilityArray(self, word: str, m: int) -> List[int]:
         ans = []
